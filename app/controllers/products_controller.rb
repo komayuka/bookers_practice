1c1cc2b1d3f350daa3054b39d666a34bb7fb5143
komayuka <komayuka115@gmail.com>
2021-04-15 08:04:15 +0000

    [product]

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,36 @@
 class ProductsController < ApplicationController
-  def new
+
+   def new
+    @product = Product.new
+   end
+
+  def create
+    @product = Product.new(product_params)
+    @product.admin_id = current_admin.id
+    @product.save
+    redirect_to product_path
   end
+
+  def index
+    @product = Product.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:product_name, :product_description, :product_img, :sales_price, :sale_status)
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,28 +2,4 @@ class Product < ApplicationRecord
   belongs_to :user
   attachment :product_img
 
-
-  def new
-    @product = Product.new
-  end
-
-  def create
-  end
-
-  def index
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
-
 end
