00aed09d6fcf6944d8addac99d55ec01150bf161
komayuka <komayuka115@gmail.com>
2021-04-16 03:34:12 +0000

    [2]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :admins, controllers: { sessions: 'admins/sessions', passwords: 'adimns/passwords', registrations: 'admins/registrations' }
+
   get 'products/new'
-  devise_for :users
+  devise_for :users, controllers: {sessions: 'users/sessions', passwords: 'users/passwords', registrations: 'users/registrations' }
   root 'homes#top'
 
   resources :products
